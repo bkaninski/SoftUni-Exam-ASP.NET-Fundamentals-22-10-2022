c1333f6ebfa085950c1465df184807b55bce09e1
bkaninski <kaninski89@abv.bg>
2022-10-23 21:44:23 +0300

    First commit

---
+++ Library/Data/Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Security.Policy;
+
+namespace Library.Data.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+
+        public List<Book> Books { get; set; } = new List<Book> ();
+    }
+}
+
+
+
---
+++ Library/Services/BookService.cs
@@ -0,0 +1,143 @@
+﻿using Library.Contracts;
+using Library.Data;
+using Library.Data.Models;
+using Library.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace Library.Services
+{
+    public class BookService : IBookService
+    {
+        private readonly LibraryDbContext context;
+
+        public BookService(LibraryDbContext _context)
+        {
+            context = _context;
+        }
+
+        public async Task AddBookAsync(AddBookViewModel model)
+        {
+            var entity = new Book()
+            {
+                Author = model.Author,
+                CategoryId = model.CategoryId,
+                ImageUrl = model.ImageUrl,
+                Rating = model.Rating,
+                Title = model.Title,
+                Description=model.Description
+            };
+
+            await context.Books.AddAsync(entity);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task AddBookToCollectionAsync(int bookId, string userId)
+        {
+            var user = await context.Users
+                .Where(u => u.Id == userId)
+                .Include(u => u.ApplicationUsersBooks)
+                .FirstOrDefaultAsync();
+
+            if (user == null)
+            {
+                throw new ArgumentException("Invalid user ID");
+            }
+
+            var book = await context.Books.FirstOrDefaultAsync(b => b.Id == bookId);
+
+            if (book == null)
+            {
+                throw new ArgumentException("Invalid Book ID");
+            }
+
+            if (!user.ApplicationUsersBooks.Any(b => b.BookId == bookId))
+            {
+                user.ApplicationUsersBooks.Add(new ApplicationUserBook()
+                {
+                    BookId = book.Id,
+                    ApplicationUserId = user.Id,
+                    Book = book,
+                    ApplicationUser = user
+                });
+
+                await context.SaveChangesAsync();
+            }
+        }
+
+        public async Task<IEnumerable<BookViewModel>> GetAllAsync()
+        {
+
+            var entities = await context.Books
+                .Include(b=>b.Category)
+                .ToListAsync();
+
+            return entities
+                .Select(b => new BookViewModel()
+                {
+                    Title = b.Title,
+                    Author = b.Author,
+                    Rating = b.Rating,
+                    Category = b?.Category?.Name,
+                    Id = b.Id,
+                    ImageUrl = b.ImageUrl
+                });
+        }
+
+        public async Task<IEnumerable<BookViewModel>> GetMyBooksAsync(string userId)
+        {
+            var user = await context.Users
+                .Where(u => u.Id == userId)
+                .Include(u => u.ApplicationUsersBooks)
+                .ThenInclude(ub => ub.Book)
+                .ThenInclude(b => b.Category)
+                .FirstOrDefaultAsync();
+
+            if (user == null)
+            {
+                throw new ArgumentException("Invalid user ID");
+            }
+
+            return user.ApplicationUsersBooks
+                .Select(b => new BookViewModel()
+                {
+                    Author = b.Book.Author,
+                    Category = b.Book.Category?.Name,
+                    Id = b.BookId,
+                    ImageUrl = b.Book.ImageUrl,
+                    Title = b.Book.Title,
+                    Rating = b.Book.Rating,
+                    Description = b.Book.Description
+                });
+        }
+
+        public async Task<IEnumerable<Category>> GetCategoriesAsync()
+        {
+            return await context.Categories.ToListAsync();
+        }
+
+        public async Task RemoveBookFromCollectionAsync(int bookId, string userId)
+        {
+            var user = await context.Users
+                .Where(u => u.Id == userId)
+                .Include(u => u.ApplicationUsersBooks)
+                .FirstOrDefaultAsync();
+
+            if (user == null)
+            {
+                throw new ArgumentException("Invalid user ID");
+            }
+
+            var book = user.ApplicationUsersBooks.FirstOrDefault(b => b.BookId == bookId);
+
+            if (book != null)
+            {
+                user.ApplicationUsersBooks.Remove(book);
+
+                await context.SaveChangesAsync();
+            }
+        }
+    }
+}
